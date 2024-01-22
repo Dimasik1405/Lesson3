60f788bcab0f96b8d414aa190a7634b92a01f9ac
Dima Malyavka <dima_malyavka@mail.ru>
2024-01-22 19:28:43 +0300

    Решение задачи 2

---
+++ Work2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[6];
+        Random rnd = new Random();
+        int count = 0;
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = rnd.Next(100, 1000);
+            Console.WriteLine(numbers[i]+" ");
+            if(numbers[i] % 2 == 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine($"Количество чётных чисел в массиве {count}");
+    }
+}
